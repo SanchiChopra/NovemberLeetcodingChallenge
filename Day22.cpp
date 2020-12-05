799334095f46f86d3033fb8c759a755091a4e4f8
Sanchi Chopra <35348387+SanchiChopra@users.noreply.github.com>
2020-12-05 18:55:28 +0530

    Unique Morse Code Words

---
+++ Day22.cpp
@@ -0,0 +1,37 @@
+/* QUESTION: International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cab" can be written as "-.-..--...", (which is the concatenation "-.-." + ".-" + "-..."). We'll call such a concatenation, the transformation of a word.
+
+Return the number of different transformations among all words we have.
+
+Example:
+Input: words = ["gin", "zen", "gig", "msg"]
+Output: 2
+Explanation: 
+The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+
+There are 2 different transformations, "--...-." and "--...--.".
+*/
+
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        string s[26] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        set<string> ms;
+        for(int i=0;i<words.size();++i){
+            string tmp="";
+            for(int j=0;j<words[i].size();++j){
+                tmp+=s[words[i][j]-'a'];
+            }
+            ms.insert(tmp);
+        }
+        return ms.size();
+    }
+};
